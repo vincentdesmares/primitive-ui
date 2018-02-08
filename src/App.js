9da065307aedac831a2fe4257833b86d119050b3
Vincent <vincent.desmares@gmail.com>
2018-02-07 23:05:42 -0800

    Work on the UI.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'tachyons/css/tachyons.min.css'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +7,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="mw-1024 pa-2 pa-6-l center">
+          <h1 className="dib mt5">See in primitives</h1>
+          <div>
+            <div className="fl-l w-50 relative">
+              <img className="w-100" src="https://github.com/vincentdesmares/node-primitive/raw/master/samples/profile.png" />
+              <div className="absolute w-100 h-100 top-0 left-0">
+                <div className="flex items-center flex-column">
+                  <div className="absolute bg-white o-70 w-30 f6 mt7 pa2 tc br4 glow pointer ba">Upload</div>
+                </div>
+              </div>
+            </div>
+            <div className="fl-l w-50"><img className="w-100" src="https://github.com/vincentdesmares/node-primitive/raw/master/samples/generated.png" /></div>
+          </div>
+        </div>
       </div>
     );
   }
