a0c216a720addd0cbdd9d0917e5d43b2672327a9
Vincent <vincent.desmares@gmail.com>
2018-02-08 08:46:30 -0800

    Remove warnings.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import 'tachyons/css/tachyons.min.css'
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -11,14 +10,14 @@ class App extends Component {
           <h1 className="dib mt5">See in primitives</h1>
           <div>
             <div className="fl-l w-50 relative">
-              <img className="w-100" src="https://github.com/vincentdesmares/node-primitive/raw/master/samples/profile.png" />
+              <img className="w-100" alt="source" src="https://github.com/vincentdesmares/node-primitive/raw/master/samples/profile.png" />
               <div className="absolute w-100 h-100 top-0 left-0">
                 <div className="flex items-center flex-column">
-                  <div className="absolute bg-white o-70 w-30 f6 mt7 pa2 tc br4 glow pointer ba">Upload</div>
+                  <div className="absolute bg-white o-70 w-30 f6 mt7 pa2 tc br4 glow pointer ba bw1">Upload</div>
                 </div>
               </div>
             </div>
-            <div className="fl-l w-50"><img className="w-100" src="https://github.com/vincentdesmares/node-primitive/raw/master/samples/generated.png" /></div>
+            <div className="fl-l w-50"><img alt="result" className="w-100" src="https://github.com/vincentdesmares/node-primitive/raw/master/samples/generated.png" /></div>
           </div>
         </div>
       </div>
